16917b9b2a87189585808db088b409f4497bc68e
Aye Yu Zin <ayeyuzin@192.168.1.15>
2025-03-08 17:21:30 +0700

    response error handling

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express"
+import { CustomError } from "../errors/custom-error";
+
+export const errorHandler = (error: Error, req : Request, res : Response, next : NextFunction) => {
+    console.log('Something went wrong', error);
+
+    if (error instanceof CustomError) {
+         res.status(error.statusCode).send({ errors: error.serializeErrors() });
+      }
+
+    res.status(400).send({
+        errors:  [ {
+            message: 'Something went wrong!'
+        }]
+    });
+}
