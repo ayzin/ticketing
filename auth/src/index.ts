6bbd62de5f8262d4f65fba70f43249b47dd78838
Aye Yu Zin <ayeyuzin@192.168.1.11>
2025-03-09 23:49:24 +0700

    jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
-
+import cookieSession from 'cookie-session';
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
@@ -12,7 +12,16 @@ import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
+app.set('trust proxy', true);
+
 app.use(json());
+
+app.use(
+    cookieSession({
+        signed: false, 
+        secure: true
+    })
+)
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -26,6 +35,12 @@ app.all('*', async (req, res) => {
 app.use(errorHandler); 
 
 const start = async () => {
+
+    if (!process.env.JWT_KEY)
+    {
+        throw new Error('JWT_KEY must be defined');
+    }
+
     try { 
         await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,11 @@
 import express, {Request, Response} from 'express'; 
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -14,13 +17,9 @@ router.post('/api/users/signup', [
     .trim()
     .isLength({min: 4, max: 20})
     .withMessage('Password must be between 4 and 20 characters')
-] , async (req : Request, res : Response) => {
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
+] ,
+validateRequest,
+async (req : Request, res : Response) => {
 
     const { email, password } = req.body;
 
@@ -33,6 +32,20 @@ router.post('/api/users/signup', [
     try {
         const user = User.build({ email, password });
         await user.save();
+
+        // generate jwt 
+        const userJwt = jwt.sign({
+                id : user.id, 
+                email: user.email
+            }, 
+            process.env.JWT_KEY!
+        );
+
+        // Store it on Session object
+        req.session = {
+            jwt: userJwt
+        };
+
         res.status(201).send(user);
     } catch (error) {
         console.error('Error saving user:', error);
